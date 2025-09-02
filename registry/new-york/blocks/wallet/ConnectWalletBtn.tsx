d22d599b4f3d2354d90116715aef50b3d22b467e
0xPratik <sariapratik@gmail.com>
2025-09-02 15:24:05 +0545

    fix : rendering problem

--- registry/new-york/blocks/wallet/ConnectWalletBtn.tsx
+++ registry/new-york/blocks/wallet/ConnectWalletBtn.tsx
@@ -11,14 +11,10 @@ export function ConnectWalletBtn() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const wallets = useWallets();
   const { isConnected } = useWallet();
-
+  console.log("isConnected", isConnected);
   const solanaWallets = wallets.filter((wallet) =>
     wallet.chains.some((chain) => chain.startsWith("solana:"))
   );
-
-  if (isConnected) {
-    return null; // Don't show connect button when already connected
-  }
 
   return (
     <>
